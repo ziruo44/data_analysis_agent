b548253617b03b22c68bbe266f54401f2b5059e2
ziruo44 <1847394002@qq.com>
2026-05-07 20:17:37 +0800

    Delete memory directory

--- memory/__init__.py
+++
@@ -1,4 +0,0 @@
-from memory.checkpointer import checkpointer
-from memory.session import get_session_config
-
-__all__ = ["checkpointer", "get_session_config"]
--- memory/checkpointer.py
+++
@@ -1,6 +0,0 @@
-from langgraph.checkpoint.sqlite import SqliteSaver
-from utils.path_tool import get_abs_path
-
-DB_PATH = get_abs_path("checkpoints.db")
-_checkpointer_cm = SqliteSaver.from_conn_string(DB_PATH)
-checkpointer = _checkpointer_cm.__enter__()
